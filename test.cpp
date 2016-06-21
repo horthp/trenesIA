3503927f3704a8d0f81a42c4dc0f320daa49b823
Patricio <patricio@localhost.localdomain>
2016-06-21 02:42:17 -0400

    avance FC subproblema  matching

--- test.cpp
+++ test.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <iostream>
 #include <cstring>
+#include <string>
 //#include <cmath>
 #include <fstream>
 //#include <ctime>
@@ -11,6 +12,10 @@
 
 using namespace std;
 
+ vector< vector<string> > arrDepSequences, arrivals, compCatDep, compCatRes, departures, facilities, gates, 
+    imposedConsumptions, initialTrains, jointArrDep, parameters, platforms, prefPlat, reuses, singleTracks, 
+    trackGroups, trainCategories, yards;
+
 typedef struct solution{
 	string train;
 	string time;
@@ -20,16 +25,12 @@ typedef struct solution{
 	string Complement;
 }sol;
 
-typedef struct depature_matching{
-	string depature;
-	//int reqDBM;
+typedef struct departures_matching{
+	string departures;
 	string train;
-	//bool arrival;
-	string arrival;
-	int cost;
-	//bool initial_train;
-	//string train_category;
-	//string train_actual_resource;
+	bool arr;
+	string arrivals;
+	float cost;
 }depMatch;
 
 typedef struct time{
@@ -101,209 +102,206 @@ vector< vector<string> > loadData(const char* filename, int value) {
     }
     return data;
 }
-
-// Se divide el problema en varios subproblemas.
-/*
-void matching_depature(vector< vector<string> > depature, vector< vector<string> > compCatDep, vector< vector<string> > initialTrains ){//, , vector< vector<string> > trainCategories, vector< vector<string> > compCatDep,  vector< vector<string> > reuses){
-	
-	int j = 1;
-	int k;
-	depMatch match_aux;
-	vector<depMatch> match;
-		for(size_t i=1;i < depature.size();i++){
-			//cout<< "for!"<<i<<endl;
-			//cout<< compCatDep[0][49] <<endl;
-			//cout << compCatDep[48][0].empty()<<"-"<< endl;
-			
-			//Se guarda la compatibilidad entre salida y categoria de tren.
-			while(depature[i][0].compare(compCatDep[j][0]) == 0 && j < compCatDep.size() - 1) {
-				match_aux.depature = compCatDep[j][0];
-				match_aux.train_category = compCatDep[j][1];
-				match_aux.reqDBM = atoi(depature[i][6].c_str());
-				match.push_back(match_aux);
-
-				if(!(compCatDep[j+1][0].empty())){
-					j++;
-				}
-
-			}
-			if(j == compCatDep.size() - 1){
-				match_aux.depature = compCatDep[j][0];
-				match_aux.train_category = compCatDep[j][1];
-				match.push_back(match_aux);
-				match_aux.reqDBM = atoi(depature[i][6].c_str());
-			}
-		}
-
-		j=1;
-		for(size_t i = 0; i < match.size(); i++){
-			//
-			//cout<<match[i].train_category.compare(initialTrains[j][1])<<endl;
-			while(match[i].train_category.compare(initialTrains[j][1]) == 0 && j < initialTrains.size() - 1){
-				cout<<"while"<<j<<endl;
-				if(atoi(initialTrains[j][3].c_str()) >= match[i].reqDBM){
-					match[i].trains.push_back(initialTrains[j][0]);
-					
-				}
-				j++;
-			}
-			//cout<<"largo trains"<< match[i].trains.size()<<endl;
-			//cout<<"salida "<<match[i].depature << " Es compatible con un tren de categoria "<< match[i].train_category<< "||reqDBM||"<<match[i].reqDBM<<endl;
-			
-			/*for(size_t k = 0; match[i].trains.size();k++){
-				cout<<"posibles trenes"<<endl;
-				cout<<match[i].trains[k]<<"->";}  */
-		//} 
-//}	
-
-void forward_checking_depature(vector< vector<string> > depature, vector< vector<string> > arrival, vector< vector<string> > init_trains, vector< vector<string> > parameters, int c){
-	//:D  Se seleciona un tren de los arrivals
-	int cost = c;
+/*Genera un domino para cada departure guardando los posibles trenes con los que pueden ser asignados y sus respectivos costos*/
+void domain_dep(vector<depMatch> &match){
+	vector< vector<string> > init_trains = initialTrains;
+	float cost = 0;
 	int difd = 0;
 	int difa = 0;
-	vector <depMatch> match;
+	int flag;
+	size_t sz;
+	//vector<depMatch> match;
 	depMatch match_aux;
 	ttime dept, arrt, restime;
 	date depd, arrd;	
 	restime = get_time(parameters[12][1]);
-	cout<<":)"<<endl;
-	for(size_t i=1; i < depature.size(); i++){
-		//cout<<"primer for- iteracion "<<i<<endl;
-		depd = get_date(depature[i][1]);
-		for(size_t j=1; j< arrival.size();j++){
-			//cout<<"segundo for- iteracion "<<j<<endl;
-			arrd = get_date(arrival[j][2]);
-			/*Se verifica que la llegada con la salida pertenescan al mismo dia 
-			y que el tiempo de salida sea mayor que el tiempo de llegada*/
-			if( depd.day == arrd.day && depd.totalmin > arrd.totalmin){
-				
-				/*Se verifica que el tiempo entre llegada y salida sea mayor o igual 
-				al tiempo minimo que se debe estar en el recuerso*/
-				if(depd.totalmin - arrd.totalmin >= restime.totalmin ){
-					dept = get_time(depature[i][5]);
-					arrt = get_time(arrival[j][8]);
-					
-					/*Se verifica que el tiempo entre llegada y salida sea menor/igual
-					 que el tiempo maximo que puede estar el tren en la plataforma tras una llegada* /
-					/*Se verifica que el tiempo entre llegada y salida sea menor/igual
-					 que el tiempo maximo que puede estar el tren en la plataforma antes de la salida */
-					if((depd.totalmin - arrd.totalmin <= arrt.totalmin) && (depd.totalmin - arrd.totalmin <=  dept.totalmin)){
-						dept = get_time(depature[i][4]);
-						arrt = get_time(arrival[j][7]);
-						match_aux.depature = depature[i][0];
-						match_aux.arrival = arrival[j][0];
-						match_aux.train = arrival[j][1];
-						if((depd.totalmin - arrd.totalmin) > arrt.totalmin){
-							difa = (depd.totalmin - arrd.totalmin) - arrt.totalmin;
-							cost = (difd*60 + difa*60)*atoi(parameters[10][0].c_str());
-						}	
-						if((depd.totalmin - arrd.totalmin) > dept.totalmin){	
-							difd = (depd.totalmin - arrd.totalmin) - dept.totalmin;
-							cost = (difd*60 + difa*60)*atoi(parameters[10][0].c_str());
-						}
+	if(departures.size()!=0){
+		for(size_t i=1; i < departures.size(); i++){
+			flag = 0; 
+			depd = get_date(departures[i][1]);
+			if(arrivals.size() != 0){
+				for(size_t j=1; j< arrivals.size();j++){
+					arrd = get_date(arrivals[j][2]);
 
-						match_aux.cost = cost;
-						cout<<"Salida "<<match_aux.depature
-						<<" cubierta por el tren "<<match_aux.train
-						<<" De la salida "<<  match_aux.arrival
-						<< " a un costo extra de "<< match_aux.cost<<endl;
-						//cout<<"i: "<<i<<" j: "<<j<<" Resta "<< depd.totalmin - arrd.totalmin<<" min "<<restime.totalmin<<" minutos"<<" Costo: "<<cost<< endl; 
+					/*Se verifica que la llegada con la salida pertenescan al mismo dia 
+					y que el tiempo de salida sea mayor que el tiempo de llegada*/
+					if( depd.day == arrd.day && depd.totalmin > arrd.totalmin){
+
+						/*Se verifica que el tiempo entre llegada y salida sea mayor o igual 
+						al tiempo minimo que se debe estar en el recuerso*/
+						if(depd.totalmin - arrd.totalmin >= restime.totalmin ){
+							dept = get_time(departures[i][5]);
+							arrt = get_time(arrivals[j][8]);
+							//cout<<departures[i][0]<<endl;
+
+							/*Se verifica que el tiempo entre llegada y salida sea menor/igual
+							 que el tiempo maximo que puede estar el tren en la plataforma tras una llegada* /
+							/*Se verifica que el tiempo entre llegada y salida sea menor/igual
+							 que el tiempo maximo que puede estar el tren en la plataforma antes de la salida */
+							if((depd.totalmin - arrd.totalmin <= arrt.totalmin) && (depd.totalmin - arrd.totalmin <=  dept.totalmin)){
+								dept = get_time(departures[i][4]);
+								arrt = get_time(arrivals[j][7]);
+								if((depd.totalmin - arrd.totalmin) > arrt.totalmin){
+									difa = (depd.totalmin - arrd.totalmin) - arrt.totalmin;
+									cost = cost + (difa*60)*atof(parameters[10][1].c_str());
+								}	
+								if((depd.totalmin - arrd.totalmin) > dept.totalmin){	
+									difd = (depd.totalmin - arrd.totalmin) - dept.totalmin;
+									cost = cost + (difd*60)*atof(parameters[10][1].c_str());
+									//cout<<"costo 2: "<<cost<<endl;
+								}
+								for(size_t k = 1;k<reuses.size();k++){
+									if(departures[i][0].compare(reuses[k][1])== 1){
+										if(arrivals[j][0].compare(reuses[k][0]) == 0){
+											cost = cost + atof(reuses[11][1].c_str());
+											//cout<<"costo 3: "<<cost<<endl;
+										}
+									}
+								}
+								match_aux.arrivals = arrivals[j][0];
+								match_aux.train = arrivals[j][1];
+								match_aux.cost = cost;
+								match_aux.departures = departures[i][0];
+								match_aux.arr = 1;
+								match.push_back(match_aux);
+
+								flag = 1;
+							}
+						}
 					}
 				}
+				if(flag == 0){
+					cout<<init_trains.size()<<endl;
+					if((init_trains.size() - 1) > 0){
+						match_aux.train = init_trains[1][0];
+						match_aux.departures = departures[i][0];
+						match_aux.arr = 0;
+						match_aux.cost = 0;
+						match.push_back(match_aux);
+						init_trains.erase(init_trains.begin() + 1);
+					}
+					else{
+						match_aux.departures = "uncov";
+						match_aux.arr = 0;
+						match_aux.arrivals = " ";
+						match_aux.train = " ";
+						match_aux.cost = atof(parameters[16][0].c_str());
+						match.push_back(match_aux);
+					}
+				}	
 			}
 		}
 	}
 }
-	//cout<< depature[1][0] << endl;
-	//Si se usa un tren originalmente en el sistema
-		//-> Enter system at d1 00:00:00 
-		//-> Enter resource
 
-	//Si se usa un tren que viene de un arrival
-		//-> Enter resource
-		//-> Exit resource
+
+void forward_checking_gbj_matching(vector<depMatch> imatch, vector<depMatch> &fmatch, float cost, int flag){
+	//int flag;
+	//float cost2;
+	for(size_t i = flag ;i<imatch.size();++i){
+		if(imatch[i].cost < cost && imatch.size() != 0){
+		   fmatch[i] = imatch[i];
+		   flag = i;
+		   cost = cost - fmatch[i].cost;
+		}
+		else{
+			
+
+		}   
+
+	}
+
+
+}
 
 void platform_assignment(){
 }
 
-void arrival_depature_sequence_assigner(){
+void arrivals_departures_sequence_assigner(){
 }
 
-void forward_checking_gbj(){
-}
+
 
 
 int main(int argc, char **argv){
 	clock_t tStart = clock();
 	string instance = argv[1];
-    vector< vector<string> > arrDepSequencesData, arrivalsData, compCatDepData, compCatResData, departuresData, facilitiesData, gatesData, 
+    /*vector< vector<string> > arrDepSequencesData, arrivalssData, compCatDepData, compCatResData, departuresData, facilitiesData, gatesData, 
     imposedConsumptionsData, initialTrainsData, jointArrDepData, parametersData, platformsData, prefPlatData, reusesData, singleTracksData, 
-    trackGroupsData, trainCategoriesData, yardsData;
+    trackGroupsData, trainCategoriesData, yardsData;*/
 
-	arrDepSequencesData = loadData((instance + "/arrDepSequences.csv").c_str(), 3);
-    arrivalsData = loadData((instance + "/arrivals.csv").c_str(), 11);
-	compCatDepData = loadData((instance + "/compCatDep.csv").c_str(), 2);
-	compCatResData = loadData((instance + "/compCatRes.csv").c_str(), 2);
-	departuresData = loadData((instance + "/departures.csv").c_str(), 8);
-	facilitiesData = loadData((instance + "/facilities.csv").c_str(), 3);
-	gatesData = loadData((instance + "/gates.csv").c_str(), 6);
-	imposedConsumptionsData = loadData((instance + "/imposedConsumptions.csv").c_str(), 7);
-	initialTrainsData = loadData((instance + "/initialTrains.csv").c_str(), 5);
-	parametersData = loadData((instance + "/parameters.csv").c_str(), 2);
-	platformsData = loadData((instance + "/platforms.csv").c_str(), 2);
-	prefPlatData = loadData((instance + "/prefPlat.csv").c_str(), 2);
-	reusesData = loadData((instance + "/reuses.csv").c_str(), 2);
-	singleTracksData = loadData((instance + "/singleTracks.csv").c_str(), 3);
-	trackGroupsData = loadData((instance + "/trackGroups.csv").c_str(), 3);
-	trainCategoriesData = loadData((instance + "/trainCategories.csv").c_str(), 7);
-	yardsData = loadData((instance + "/yards.csv").c_str(), 2);	
+	arrDepSequences = loadData((instance + "/arrDepSequences.csv").c_str(), 3);
+    arrivals = loadData((instance + "/arrivals.csv").c_str(), 11);
+	compCatDep = loadData((instance + "/compCatDep.csv").c_str(), 2);
+	compCatRes = loadData((instance + "/compCatRes.csv").c_str(), 2);
+	departures = loadData((instance + "/departures.csv").c_str(), 8);
+	facilities = loadData((instance + "/facilities.csv").c_str(), 3);
+	gates = loadData((instance + "/gates.csv").c_str(), 6);
+	imposedConsumptions = loadData((instance + "/imposedConsumptions.csv").c_str(), 7);
+	initialTrains = loadData((instance + "/initialTrains.csv").c_str(), 5);
+	parameters = loadData((instance + "/parameters.csv").c_str(), 2);
+	platforms = loadData((instance + "/platforms.csv").c_str(), 2);
+	prefPlat = loadData((instance + "/prefPlat.csv").c_str(), 2);
+	reuses = loadData((instance + "/reuses.csv").c_str(), 2);
+	singleTracks = loadData((instance + "/singleTracks.csv").c_str(), 3);
+	trackGroups = loadData((instance + "/trackGroups.csv").c_str(), 3);
+	trainCategories = loadData((instance + "/trainCategories.csv").c_str(), 7);
+	yards = loadData((instance + "/yards.csv").c_str(), 2);	
 
-    //cout<< initialTrainsData[1][4] << endl;
-	//matching_depature(departuresData,compCatDepData, initialTrainsData);
-	 /*cout<<departuresData[2][1][0]<<endl
-		<<departuresData[2][1][1]<<endl
-		<<departuresData[2][1][2]<<endl
-		<<departuresData[2][1][3]<<endl
-		<<departuresData[2][1][4]<<endl
-		<<departuresData[2][1][5]<<endl
-		<<departuresData[2][1][6]<<endl
-		<<departuresData[2][1][7]<<endl
-		<<departuresData[2][1][8]<<endl
-		<<departuresData[2][1][9]<<endl
-		<<departuresData[2][1][10]<<endl
-		<<departuresData[2][1][11]<<endl; */
-
-	vector <int> test;
-	/*
-	int k = 0;
-	for(k=0;k<10;k++){
-		test.push_back(k);
+ 
+	vector <depMatch> test,final;
+	float cost = 0;
+	depMatch m;
+	m.departures = " ";
+	m.cost = 0;
+	//cout<<arrivals.size()<<endl;
+	domain_dep(test);
+	cout<<"Dominio depX"<<endl;
+	for(size_t l=0;l<test.size();l++){
+		cost = cost + test[l].cost;
+		cout<<"departures: "<<test[l].departures<<"|| train: "<<test[l].train<<" || de arrivals: "
+		<<test[l].arrivals<< " || costo: "<< test[l].cost<< endl;
 	}
-	
-	test.pop_back();
-	for(size_t i = 0; i<test.size();i++)
-		cout<<test[i]<<endl;*/
-	//string day = departuresData[1][1][0].c_str() + departuresData[1][1][1].c_str();
-	//cout<< day <<endl;
-	//date test;
+	cout<<"Costo total "<<cost<<endl;
+	/*/cout<< "D"<<endl;	
+	/
+	date d;
 	ttime t;
-	//cout<<"T"<<endl;
-	//t = get_time(departuresData[4][5]);
-	/*
-	cout<<"hora "<<t.hh<<endl;
-	cout<<"min " <<t.mm<<endl;
-	cout<<"seg " <<t.ss<<endl;
-	cout<<"minutos totales " <<t.totalmin<<endl; */
-	forward_checking_depature(departuresData,arrivalsData,initialTrainsData,parametersData,0);
+	cout<< "departures" <<endl;
+	d = get_date(departuresData[1][1]);
+	t = get_time(departuresData[1][5]);
+	cout<<"date 1 "<< d.totalmin <<" Totaldwell " << t.totalmin<<endl;
+	d = get_date(departuresData[2][1]);
+	t = get_time(departuresData[2][5]);
+	cout<<"date 2 "<< d.totalmin <<" Totaldwell " << t.totalmin<<endl; 
+	d = get_date(departuresData[3][1]);
+	t = get_time(departuresData[3][5]);
+	cout<<"date 3 "<< d.totalmin <<" Totaldwell " << t.totalmin<<endl; 
+	d = get_date(departuresData[4][1]);
+	t = get_time(departuresData[4][5]);
+	cout<<"date 4 "<< d.totalmin <<" Totaldwell " << t.totalmin<<endl; 
+	d = get_date(departuresData[5][1]);
+	t = get_time(departuresData[5][5]);
+	cout<<"date 5 "<< d.totalmin <<" Totaldwell " << t.totalmin<<endl;
 
-	//cout<< "D"<<endl;	
+	cout<<endl<<"arrivalss"<<endl; 
+	d = get_date(arrivalssData[1][2]);
+	t = get_time(arrivalssData[1][8]);
+	cout<<"date 1 "<< d.totalmin <<"Totaldwell " << t.totalmin<<endl;
+	d = get_date(arrivalssData[2][2]);
+	t = get_time(arrivalssData[2][8]);
+	cout<<"date 2 "<< d.totalmin <<"Totaldwell " << t.totalmin<<endl; 
+	d = get_date(arrivalssData[3][2]);
+	t = get_time(arrivalssData[3][8]);
+	cout<<"date 3 "<< d.totalmin <<" totaldwell " << t.totalmin<<endl; 
+	d = get_date(arrivalssData[4][2]);
+	t = get_time(arrivalssData[4][8]);
+	cout<<"date 4 "<< d.totalmin <<"Totaldwell " << t.totalmin<<endl; 
+	d = get_date(arrivalssData[5][2]);
+	t = get_time(arrivalssData[5][8]);
+	cout<<"date 5 "<< d.totalmin <<" Totaldwell " << t.totalmin<<endl;  
+	*/
 	
-	/*
-	test = get_date(departuresData[2][1]);
-	cout<<"Dia " <<test.day<<endl;
-	cout<<"hora "<<test.hh<<endl;
-	cout<<"min " <<test.mm<<endl;
-	cout<<"seg " <<test.ss<<endl; */
 	cout<< "la ejecucion fue de: "<<(double)(clock() - tStart)/CLOCKS_PER_SEC<<" segundos"<<endl; 
 	return 0;
 }
